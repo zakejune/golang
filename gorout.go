f87de9cff73801a5ade3108f7e6630deecb76b22
zakejune <juneyoung1204@outlook.com>
2019-07-29 16:13:59 +0800

    first commit

---
+++ gorout.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func count() {
+    for i:=0;i<5;i++{
+        fmt.Println(i)
+        time.Sleep(time.Millisecond*1)
+    }
+}
+
+func main() {
+    go count()
+    time.Sleep(time.Millisecond*2)
+    fmt.Println("hello")
+    time.Sleep(time.Millisecond*5)
+}
+
